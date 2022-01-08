981069f7604a808b12eb1985fd2cf625874911f1
tianliu <tianal@8afbf97c.st-andrews.ac.uk>
2022-01-08 22:56:31 +0000

    update our menu-item

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  rootElement
 );
 
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import Directory from "../../components/directory/directory.components";
 
 const HomePage = () => (
     <div className="homepage">
-        <Directory />
+        <Directory  />
     </div>
 );
 
